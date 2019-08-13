54840d855db6c49d623a0201e168323066fa7ced
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-13 22:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ function add(a,b) {
   c = a + b
   return c
 }
+
+function subtract(a,b) {
+  c = a - b
+  return c
+}
+
+function multiply(a,b) {
+  c = a*b
+  return c
+}
+
+function divide(a,b) {
+  c = a/b
+  return c
+}
+
+function add(a,b) {
+  c = a + b
+  return c
+}
